cf8bb18f8f96d2c7664dfea7ecf4a16c556bc5a0
Roshan Kumar <roshankumarxc@gmail.com>
2024-06-15 17:07:48 +0530

    car commands

---
+++ car game.py
@@ -0,0 +1,18 @@
+com = ""
+while com.lower() != "quit":
+    com = input("> ")
+    if com.lower() == "start":
+        print("car started...")
+    elif com.lower() == "stop":
+        print("car stopped...")  
+    elif com == "help":
+        print("""
+        start - to start the car
+        stop - to stop the car 
+        quit - to quit""")
+    else:
+        print("command invalid")          
+
+
+
+
