437b108e148f66650c485fe9b276c433f8ade9f3
Roshan Kumar <roshankumarxc@gmail.com>
2024-06-23 14:11:10 +0530

    dictionary function

---
+++ Dictionaries.py
@@ -0,0 +1,7 @@
+customer = {"name":"John", "age" : 30, "si" : True}
+
+customer["name"] = "Jack Sparrow"
+customer["bithdate"] = "September 11 2001"
+
+print(customer["name"])
+print(customer["bithdate"])
