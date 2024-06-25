6199ab3302caf6d7ede0e20378150af0cc0808c8
Roshan Kumar <roshankumarxc@gmail.com>
2024-06-25 13:57:38 +0530

    keyword arg

---
+++ keyword arguments.py
@@ -0,0 +1,9 @@
+def greet_user(name,last_name):
+    print(f"hi there {name} {last_name}")
+    print("welcome aboard")
+
+print("start")
+greet_user(name="john",last_name="smith")
+
+
+print("finish")
