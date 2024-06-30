df109943108e108a9da153061a72827a214c9eee
Roshan Kumar <roshankumarxc@gmail.com>
2024-06-30 23:08:45 +0530

    AUTOMATION PROJECT

--- excel spreadsheets.py
+++ excel spreadsheets.py
@@ -1,6 +1,17 @@
 import openpyxl as xl
+import os
 
-wb = xl.load_workbook('pyhton excel.xlsx')
-sheet = wb['Sheet1']
-cell = sheet['a1']
-print(cell)
+#print the current working directory
+print("current working directory: ", os.getcwd())
+
+#list of all the files in the current working directory
+print("files in the current directory: ",os.listdir())
+
+#provide the absolute path to the file 
+file_path = os.path.abspath('python excel.xlsx')
+print("file path: ",file_path)
+
+wb = xl.load_workbook('test.xlsx')
+sheet = wb['Sheet']
+cell = sheet['A1']
+print(cell.value)
